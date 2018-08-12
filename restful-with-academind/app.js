3dfc579abeb3852ba65cb05b4e27248cf9e97e45
rage <drage@me.com>
2018-08-12 14:59:46 +0100

    finished lesson 6

--- restful-with-academind/app.js
+++ restful-with-academind/app.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
+const morganLogging = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./controllers/products');
 const orderRoutes = require('./controllers/orders');
 
+mongoose.connect('mongodb://raged_node_shop_0317:'+
+        process.env.MONGO_ATLAS_PW+
+        '@cluster0-shard-00-00-goznt.mongodb.net:27017,cluster0-shard-00-01-goznt.mongodb.net:27017,cluster0-shard-00-02-goznt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
+
+app.use(morganLogging('dev'));
+// the following line will allow this app to parse simple type, denote by extended: false
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next)=>{
+    const error = new Error('Not found');
+    error.status(404);
+    next(error);
+});
+
+
+app.use((error, req, res, next)=>{
+   res.status(error.status || 500);
+   res.json({
+       error:{message: error.message}
+   });
+});
 
 module.exports = app;
--- restful-with-academind/controllers/products.js
+++ restful-with-academind/controllers/products.js
@@ -1,28 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../api/models/product');
 
-router.get('/', (req, res, next) =>{
+router.get('/', (req, res, next) => {
     res.status(200).json({
-        message:'handling GET request to / products'
+        message: 'handling GET request to / products'
     });
 });
 
-router.post('/', (req, res, next) =>{
+router.post('/', (req, res, next) => {
+    const product = new Product({
+        id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+
     res.status(201).json({
-        message:'handling POST request to / products'
+        message: 'handling POST request to / products'
     });
 });
 
 // note the semicolon this denote its a parameter
-router.get('/:productID', (req, res, next) =>{
+router.get('/:productID', (req, res, next) => {
     const id = req.params.productID;
-    if(id === 'special'){
+    if (id === 'special') {
         res.status(200).json({
-            message:'you discovered the special ID', ID:id
+            message: 'you discovered the special ID', ID: id
         });
-    }else{
+    } else {
         res.status(200).json({
-            message:'you aint getting an ID'
+            message: 'you aint getting an ID'
         });
 
     }
