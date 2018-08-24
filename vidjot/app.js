71bacf534fbd3158805ce1af4b9f49e7859ecf53
rage <drage@me.com>
2018-08-24 19:21:56 +0100

    video todo list

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,23 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const port = 5000;
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 
+const app = express();
 
+//declare routes
+const aboutRoute = require('./controllers/routes/about');
+const indexRoute = require('./controllers/routes/index');
+const ideasRoute = require('./controllers/routes/ideas');
 
 //connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
     useNewUrlParser: true
 }).then(() => console.log('MongoDB Connected ...'))
     .catch(err => console.log(err))
-;
+    ;
 
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
-
-const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -25,14 +29,9 @@ app.engine('handlebars', exphbs({
 }))
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
+app.use('/', indexRoute);
+app.use('/about',aboutRoute)
+app.use('/ideas', ideasRoute)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
---
+++ vidjot/controllers/routes/ideas.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+require('../../models/Idea');
+const Idea = mongoose.model('ideas');
+
+router.get('/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+router.post('/', (req, res) => {
+    let errors = [];
+
+    if (!req.body.title) {
+        errors.push({ text: 'Please add a title' });
+    }
+
+    if (!req.body.details) {
+        errors.push({ text: 'Please add some details' });
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+
+        console.log(newUser.title + " " + newUser.details)
+
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            });
+    }
+});
+
+module.exports = router;
